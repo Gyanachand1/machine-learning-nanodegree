cd7bbd48b53dd746fb63b37227f5a70c311f8a4a
Mabel VJ <mabelvj@gmail.com>
2016-08-31 04:59:18 +0200

    casa 30-8-16 END

--- P4_smartcab/smartcab/agent.py
+++ P4_smartcab/smartcab/agent.py
@@ -2,6 +2,8 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import numpy as np
+import itertools
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -11,28 +13,59 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-
+        self.list_of_actions = [ None, 'forward', 'left', 'right'] 
+        self.state = [] #we will use a list of tuples of states to map the indexes
+        self.next_waypoint = None
+        self.number_of_states= len(self.list_of_actions)*2*2*2*2# 4 waypoints or actions * 2 light states * 2 oncoming  traffic states * 2 left traffic states  * 2 right  traffic states 
+        self.R = np.zeros(shape=[self.number_of_states, 4]) #we have 4 actions
+        self.q = np.zeros(shape=[self.number_of_states, 4])
+        #self.q = np.empty(shape=[self.number_of_states,self.number_of_states])
+        self.state_counter = 0
+        self.gamma = .8 #random value
+		
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-
-    def update(self, t):
-        # Gather inputs
-        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
-        inputs = self.env.sense(self)
-        deadline = self.env.get_deadline(self)
-
-        # TODO: Update state
+        self.color = 'red'  # override color
+        #self.state = []
+        self.next_waypoint = None
         
-        # TODO: Select action according to your policy
-        action = None
-
-        # Execute action and get reward
-        reward = self.env.act(self, action)
-
-        # TODO: Learn policy based on state, action, reward
-
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+    def update(self, t):
+		# Gather inputs
+		self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+		inputs = self.env.sense(self)
+		deadline = self.env.get_deadline(self)
+		
+		# TODO: Update state
+		this_state= (inputs['light'], inputs['oncoming'], inputs['right'], inputs['left'], self.next_waypoint)
+		
+		if not this_state in self.state:
+			self.state.append(this_state)
+		print self.state
+			
+		#if not self.state  or not (inputs) in self.state: # if dictionary if empty : initialize it, if it has not the state: add it and increase the counter.
+			#self.state[(inputs)] = self.state_counter
+			#self.state_counter += 1
+		
+		# TODO: Select action according to your policy
+		action = random.choice(self.list_of_actions)
+		
+		# Execute action and get reward
+		reward = self.env.act(self, action)
+		#self.R.resize(self.R.shape[0]+1, self.R.shape[1]+1)
+		self.R[self.state.index(this_state)][self.list_of_actions.index(action)] = reward
+		#print self.R
+		# TODO: Learn policy based on state, action, reward
+		
+		#Q = reward + Gamma * np.max[[ in list_of_actions]]	
+		#print self.q
+		temp = []
+		for (next_state,next_action) in list(itertools.product(range(self.number_of_states),self.list_of_actions)):
+			temp.append( self.q[next_state][self.list_of_actions.index(next_action)]) 
+		self.q[self.state.index(this_state)][self.list_of_actions.index(action)] = reward + self.gamma* np.max(temp)
+				# we iterate over the possible combinations of states and actions, since any future combination is possible 
+		print self.q[range(len(self.state))][:]
+		print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
 def run():
