e94456f92bb68b1e32b0482fc603188967abd8f7
Mabel VJ <mabelvj@gmail.com>
2016-09-05 15:44:48 +0200

    train 5-9-16 1

--- P4_smartcab/smartcab/agent.py
+++ P4_smartcab/smartcab/agent.py
@@ -26,10 +26,9 @@ class LearningAgent(Agent):
 		self.R = np.zeros(shape=[self.number_of_states, len(self.valid_actions)]) #we have 4 actions
 		self.q = np.zeros(shape=[self.number_of_states, len(self.valid_actions)])
 		self.policy = np.zeros([self.number_of_states, 1], dtype = int)
-		#self.q = np.empty(shape=[self.number_of_states,self.number_of_states])
 		self.state_counter = 0
-		self.alpha = 1 # learning rate		
-		self.gamma = .5 #discount factor
+		self.alpha = .95 # learning rate		
+		self.gamma = .1 #discount factor
 		self.epsilon = .01
 		self.data = pd.DataFrame({'alpha' : self.alpha, 'gamma' : self.gamma, 'epsilon' : self.epsilon,'successful': 0,'infractions' : 0, 'Q': [self.q], 'R': [self.R]})
 		print self.data
@@ -124,10 +123,6 @@ class LearningAgent(Agent):
 		this_state =  self.return_state(inputs, self.next_waypoint)
 		self.store_states(this_state)
 		self.state.append(self.list_of_states.index(this_state))
-		
-			#print self.list_of_states
-		#print self.state
-	
 	
 		# TODO: Select action according to your policy
 		action = self.select_action()
@@ -137,16 +132,16 @@ class LearningAgent(Agent):
 		# MODIFICATION https://discussions.udacity.com/t/efficiently-counting-the-number-of-times-car-reaches-destination-in-last-10-trials-out-of-100/174080/2
 		self.get_reward(reward,t)
 		self.R[self.state[-1]][self.valid_actions.index(action)] = reward
-		#print self.R[range(len(self.list_of_states))][:]
+			#print self.R[range(len(self.list_of_states))][:]
 		
 		# TODO: Learn policy based on state, action, reward
 		self.update_q(action,t+1.)
-		#print self.q[range(len(self.list_of_states))][:]
+			#print self.q[range(len(self.list_of_states))][:]
 		
 		#we select the maximum argument from the list of possible actions for the state
 		self.get_policy()
 
-		#print self.policy[range(len(self.list_of_states))]
+				#print self.policy[range(len(self.list_of_states))]
 				
 		print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -158,28 +153,23 @@ class LearningAgent(Agent):
 		next_state = self.return_state(next_inputs, next_waypoint)
 		temp = []
 		self.store_states(next_state)
-		print next_state, self.list_of_states.index(next_state) 
 		temp= [self.q[self.list_of_states.index(next_state)][self.valid_actions.index(next_action)] for next_action in self.valid_actions] 
-		# First approach : Q_hat(s,a) += alpha(r+gamma*max(abs(Q_hat(s',a')-Q_hat(s,a))))
+		# First approach : Q_hat(s,a) = alpha(r+gamma*max(abs(Q_hat(s',a')-Q_hat(s,a))))
 		self.alpha = 1./t
 		self.q[self.state[-1]][self.valid_actions.index(action)]+= self.alpha* (reward +  self.gamma*np.max(temp-(1./self.gamma)*self.q[self.state[-1]][self.valid_actions.index(action)]))
 				# we iterate over the possible combinations of states and actions, since any future combination is possible 
-		#print self.q[range(len(self.list_of_states))][:]
-		#self.list_of_states.pop() # we delete the state we have added just to not make confussion for the update script	
 	def select_action(self):
 		if len(self.state)==1: # if we are in the first iteration 
 		#1: random action
 			action = random.choice(self.valid_actions)
-		#1 next waypoint can be a good direction to start
+		#1.b:  next waypoint can be a good direction to start, there is no policy yet
 			action= self.next_waypoint
 		else:
 		#2_using policy to take an action
 			probabilities = [1.-self.epsilon  , self.epsilon/float(len(self.valid_actions)) , self.epsilon/float(len(self.valid_actions)) , self.epsilon/float(len(self.valid_actions)), self.epsilon/float(len(self.valid_actions))]
-			#print probabilities
 			#probabilities = [1-e 0.25*e 0.25*e 0.25*e 0.25*e]
 			these_actions = []						
 			these_actions = [self.valid_actions[self.policy[self.state[-2]]]]  + self.valid_actions
-			#print these_actions
 			action =  np.random.choice(these_actions, 1, p= probabilities)[0]#we access the state previous to the present one in order to choose the action to take 
 		return action
 		
@@ -214,10 +204,10 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-	sim = Simulator(e, update_delay=0.1, display=True)  # create simulator (uses pygame when display=True, if available)
+	sim = Simulator(e, update_delay=0., display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-	sim.run(n_trials=100)#run for a specified number of trials
+	sim.run(n_trials=250)#run for a specified number of trials
 	a.write_state_to_csv()
 	
 	# NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
